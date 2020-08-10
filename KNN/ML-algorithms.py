b08edef60c9637d8a3651797fa9de625de94a8a3
David <voihanskidavid@gmail.com>
2020-08-10 14:14:42 +0300

    code cleanup

--- KNN/ML-algorithms.py
+++ KNN/ML-algorithms.py
@@ -11,6 +11,7 @@ from keras.utils import to_categorical
 from sklearn.naive_bayes import MultinomialNB
 import time
 import pickle
+from sklearn.preprocessing import MultiLabelBinarizer
 import matplotlib.pyplot as plt
 from PIL import Image
 
@@ -68,7 +69,7 @@ def KNN(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x: np.ndar
     save_model(save_path, classifier)
     # test model
     y_pred = classifier.predict(test_data_x)
-    print("KNN accuracy: ", np.mean(y_pred == test_data_y))
+    print("KNN accuracy: ", np.mean(y_pred == test_data_y) * 100, "%")
 
 
 def SVM(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x: np.ndarray, test_data_y: np.ndarray):
@@ -84,14 +85,14 @@ def SVM(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x: np.ndar
 
     print("SVM started...")
     # fit model
-    clf = svm.SVC(decision_function_shape='ovo')
+    clf = svm.SVC(decision_function_shape='ovo', gamma="scale")
     clf.fit(train_data_x, train_data_y)
     # save model
     save_path = 'saved-models/svm-model-train.sav'
     save_model(save_path, clf)
     # test model
-    pred_y = clf.predict(test_data_x)
-    print("SVM accuracy: ", np.mean(pred_y == test_data_y))
+    score = clf.score(test_data_x, test_data_y)
+    print("SVM accuracy: ", score * 100, "%")
 
 
 def naive_bayes(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x: np.ndarray, test_data_y: np.ndarray):
@@ -110,31 +111,11 @@ def naive_bayes(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x:
     clf = MultinomialNB()
     clf.fit(train_data_x, train_data_y)
     # save model
-    save_path = 'saved-models/nb-model-train.sav'
+    save_path = 'saved-models/naive-bayes-model-train.sav'
     save_model(save_path, clf)
     # test model
-    pred_y = clf.predict(test_data_x)
-    print("Naive-Bayes accuracy: ", np.mean(pred_y == test_data_y))
-
-    tsne = TSNE().fit_transform(pred_y)
-    tx, ty = tsne[:, 0], tsne[:, 1]
-    tx = (tx - np.min(tx)) / (np.max(tx) - np.min(tx))
-    ty = (ty - np.min(ty)) / (np.max(ty) - np.min(ty))
-
-    width = 4000
-    height = 3000
-    max_dim = 100
-    full_image = Image.new('RGB', (width, height))
-    for idx, x in enumerate(x_test):
-        tile = Image.fromarray(np.uint8(x * 255))
-        rs = max(1, tile.width / max_dim, tile.height / max_dim)
-        tile = tile.resize((int(tile.width / rs),
-                            int(tile.height / rs)),
-                           Image.ANTIALIAS)
-        full_image.paste(tile, (int((width - max_dim) * tx[idx]),
-                                int((height - max_dim) * ty[idx])))
-    plt.figure(figsize=(16, 12))
-    plt.imshow(full_image)
+    score = clf.score(test_data_x, test_data_y)
+    print("Naive-Bayes accuracy: ", score * 100, "%")
 
 
 def dec_tree(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x: np.ndarray, test_data_y: np.ndarray):
@@ -153,11 +134,11 @@ def dec_tree(train_data_x: np.ndarray, train_data_y: np.ndarray, test_data_x: np
     clf = tree.DecisionTreeClassifier()
     clf.fit(train_data_x, train_data_y)
     # save model
-    save_path = 'saved-models/d-tree-model-train.sav'
+    save_path = 'saved-models/decision-tree-model-train.sav'
     save_model(save_path, clf)
     # test model
-    pred_y = clf.predict(test_data_x)
-    print("Dec' tree accuracy: ", np.mean(pred_y == test_data_y))
+    score = clf.score(test_data_x, test_data_y)
+    print("Dec' tree accuracy: ", score * 100, "%")
 
 
 def test_using_storage(saved_model_path: str, test_data_x: np.ndarray, test_data_y: np.ndarray):
@@ -168,14 +149,16 @@ def test_using_storage(saved_model_path: str, test_data_x: np.ndarray, test_data
     :param test_data_y: Test labels list
     """
     print(saved_model_path.split('/')[1], "started")
+    # load model
     loaded_model = pickle.load(open(saved_model_path, 'rb'))
+    # test model
     score = loaded_model.score(test_data_x, test_data_y)
     print("Accuracy: {0:.2f} %".format(100 * score))
 
 
 if __name__ == '__main__':
     start_time = time.time()
-
+    
     # get the features vector (250 features)
     model = restore_model()
     layer_name = 'dense_2'
@@ -184,26 +167,26 @@ if __name__ == '__main__':
 
     # load and prepare the data
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    y_train_one_hot = to_categorical(y_train)
-    y_test_one_hot = to_categorical(y_test)
-
+    mlb = MultiLabelBinarizer()
+    # pre processing
+    y_train_one_hot = mlb.fit_transform(y_train)
+    y_test_one_hot = mlb.fit_transform(y_test)
     step1 = input("type t to train the whole model, or any for testing using storage\n")
     print()
-    x_test = encode(x_test, encoder_layer)
 
+    x_test = encode(x_test, encoder_layer)
     if step1 == 't':
         x_train = encode(x_train, encoder_layer)
         # run the algorithms
-        # KNN(x_train, y_train_one_hot, x_test, y_test_one_hot)
-        # SVM(x_train, y_train.flatten(), x_test, y_test.flatten())
+        KNN(x_train, y_train_one_hot, x_test, y_test_one_hot)
+        SVM(x_train, y_train.flatten(), x_test, y_test.flatten())
         naive_bayes(x_train, y_train.flatten(), x_test, y_test.flatten())
-        # dec_tree(x_train, y_train_one_hot, x_test, y_test_one_hot)
+        dec_tree(x_train, y_train_one_hot, x_test, y_test_one_hot)
     else:
-
-        # test_using_storage('saved-models/nb-model-train.sav', x_test, y_test)
-        # test_using_storage('saved-models/knn-model-train.sav', x_test, y_test)
+        test_using_storage('saved-models/naive-bayes-model-train.sav', x_test, y_test)
+        test_using_storage('saved-models/knn-model-train.sav', x_test, y_test_one_hot)
         test_using_storage('saved-models/svm-model-train.sav', x_test, y_test)
-        # test_using_storage('saved-models/d-tree-model-train.sav', x_test, y_test)
+        test_using_storage('saved-models/decision-tree-model-train.sav', x_test, y_test_one_hot)
 
     print("--- %s minutes ---" % ((time.time() - start_time) / 60))
-    gc.collect()    # dump memory
+    gc.collect()  # dump memory
